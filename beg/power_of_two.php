868727a111718deb5388e29f26e9f21eeb24aafb
Turston <turstonds@gmail.com>
2021-11-15 20:03:04 +0200

    first commit

---
+++ beg/power_of_two.php
@@ -0,0 +1,11 @@
+<?php
+
+    $input = 5;
+
+    if($input % 2 == 0) {
+        echo "<h3> {$input} is divisible by two.</h3>";
+    } else {
+        echo "<h3> {$input} not divisible by two.</h3>";
+    }
+
+?>
