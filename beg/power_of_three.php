adc14e6ff3efe574931446deb458a88ccd5ffee5
Turston <turstonds@gmail.com>
2021-11-16 11:54:06 +0200

    check if input is a power of three

---
+++ beg/power_of_three.php
@@ -0,0 +1,11 @@
+<?php
+
+    $input = 0;
+
+    if($input % 3 == 0){
+        echo "<p>{$input} is a power of three.</p>";
+    }else{
+        echo "<p>{$input} is not a power of three.</p>";
+    }
+
+?>
